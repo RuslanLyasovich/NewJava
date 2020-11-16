18e634b3edee0f399ea0a4b551f508557e0d61f7
Ruslan Lyasovich <Ruslam_Lyasovich@epam.com>
2020-11-16 14:10:54 +0300

    This is an attempt to organize everything

---
+++ src/main/java/com/automation/fundamentals_optional/JavaFundamentalOptionalTask.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class JavaFundamentalOptionalTask {
+
+    public static void main(String[] args) {
+
+        // Ввести n чисел с консоли
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите числа через пробел: ");
+        String input = scan.nextLine();
+
+        // Найти самое короткое и самое длинное число. Вывести найденные числа и их длину.
+        StringLengthSort.theLongAndShortNombers (input);
+
+
+        // Сортировка по возрастанию и убыванию длины чисел
+        String[] numbers = input.split(" ");
+        StringLengthSort.allNumbers(numbers);
+        StringLengthSort.toLongNumbers(numbers);
+        StringLengthSort.toShortNumbers(numbers);
+    }
+}
+
+
+
